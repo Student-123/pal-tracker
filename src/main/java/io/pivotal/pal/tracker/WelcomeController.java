1ce46ce6f43cf0045a4914e442c7a5cbffa969b7
Santhosh A <studentnumber123@gmail.com>
2019-06-17 16:02:36 +0530

    PalTracker - Travis to jdk 8

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
     Route: pal-tracker-grouchy-gorilla.cfapps.io
     cf push pal-tracker -p pal-tracker.jar --random-route
     993784f0c85466f12cf98dc65da8ff33cd0791df
+    67d1ac38b7189010db15566d4bbae5b5c1d965c2
        JBP_CONFIG_OPEN_JDK_JRE: '{ jre: { version: 11.+ } }'
+       GITHUB_OAUTH_TOKEN
+       GITHUB_USERNAME
  */
 @RestController
 public class WelcomeController {
